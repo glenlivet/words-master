13f39e01fa68931dede738ebaafce4937b3c3f12
Zhang, Shulai <Shulai.Zhang@sungard.com>
2015-01-04 18:41:55 +0800

    1 version done

--- test/service-test.js
+++ test/service-test.js
@@ -4,13 +4,27 @@
  var backService = new WmService();
 
  backService.start(function(){
- 	var session = {
- 		username : 'oll'
- 	};
- 	var reviewBundle = {
- 		words : [],
- 		page : 0,
- 		wordsPerPage : 10
- 	};
- 	backService.queryReview(session, reviewBundle);
+ 	// var session = {
+ 	// 	username : 'oll'
+ 	// };
+ 	// var reviewBundle = {
+ 	// 	words : [],
+ 	// 	page : 0,
+ 	// 	wordsPerPage : 10
+ 	// };
+ 	// backService.queryReview(session, reviewBundle);
+
+ 	testHandleWord(backService);
  });
+
+
+ var testHandleWord = function(bs){
+ 	var session = {
+ 		username : 'kikyou'
+ 	};
+ 	bs.on('wordExisted', function(w){
+ 		console.error(w + ' has already existed!');
+ 	});
+ 	bs.handleWord(session, 'impossible');
+
+ }
